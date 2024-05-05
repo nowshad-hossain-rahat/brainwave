5c99ea9cccc6cf082a3b05a60f868de557f72bff
MD Morsed Alam <mdmorsedalam68@gmail.com>
2024-05-05 13:06:12 +0600

    Hero section half done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,21 +22,21 @@ const Hero = () => {
       id="hero"
     >
       <div className="section-container" ref={parallaxRef}>
-        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
-          <h1 className="h1 mb-6">
+        <div className="section-content">
+          <h1 className="section-heading">
             Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
-            <span className="inline-block relative">
+            <span className="section-heading-span">
               Brainwave{" "}
               <img
                 src={curve}
-                className="absolute top-full left-0 w-full xl:-mt-2"
+                className="section-heading-span-img"
                 width={624}
                 height={28}
                 alt="Curve"
               />
             </span>
           </h1>
-          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+          <p className="section-peragraph">
             Unleash the power of AI within Brainwave. Upgrade your productivity
             with Brainwave, the open AI chat app.
           </p>
