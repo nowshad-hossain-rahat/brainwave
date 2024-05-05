c323c849f0449bb4c8eb5669742dc96155303969
MD Morsed Alam <mdmorsedalam68@gmail.com>
2024-05-05 18:45:50 +0600

    working on gradient component

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -20,19 +20,17 @@ const Notification = ({
         className="icon"
       />
 
-      <div className="flex-1">
-        <h6 className="mb-1 font-semibold text-base">{title}</h6>
+      <div className="notification-right-side">
+        <h6 className="heading">{title}</h6>
 
-        <div className="flex items-center justify-between">
-          <ul className="flex -m-0.5">
+        <div className="profile-container">
+          <ul>
             {notificationImages.map((item, index) => (
               <li
                 key={index}
-                className="flex w-6 h-6 border-2 border-n-12 rounded-full overflow-hidden"
               >
                 <img
                   src={item}
-                  className="w-full"
                   width={20}
                   height={20}
                   alt={item}
@@ -40,7 +38,7 @@ const Notification = ({
               </li>
             ))}
           </ul>
-          <div className="body-2 text-n-13">1m ago</div>
+          <div className="time-text">1m ago</div>
         </div>
       </div>
     </div>
--- src/components/design/HeroComponents.tsx
+++ src/components/design/HeroComponents.tsx
@@ -8,8 +8,8 @@ import "../../assets/scss/Hero.scss";
 export const Gradient = () => {
   return (
     <>
-      <div className="relative z-1 h-6 mx-2.5 bg-n-11 shadow-xl rounded-b-[1.25rem] lg:h-6 lg:mx-8" />
-      <div className="relative z-1 h-6 mx-6 bg-n-11/70 shadow-xl rounded-b-[1.25rem] lg:h-6 lg:mx-20" />
+      <div className="gradient-first-div shadow-xl" />
+      <div className="gradient-second-div shadow-xl" />
     </>
   );
 };
@@ -17,11 +17,11 @@ export const Gradient = () => {
 export const BottomLine = () => {
   return (
     <>
-      <div className="hidden absolute top-[55.25rem] left-10 right-10 h-0.25 bg-n-6 pointer-events-none xl:block" />
+      <div className="bottomLine-div pointer-events-none xl:block" />
 
-      <PlusSvg className="hidden absolute top-[54.9375rem] left-[2.1875rem] z-2 pointer-events-none xl:block" />
+      <PlusSvg className="bottomLine-plussvg" />
 
-      <PlusSvg className="hidden absolute top-[54.9375rem] right-[2.1875rem] z-2 pointer-events-none xl:block" />
+      <PlusSvg className="bottomLine-plussvg" />
     </>
   );
 };
@@ -29,10 +29,10 @@ export const BottomLine = () => {
 const Rings = () => {
   return (
     <>
-      <div className="absolute top-1/2 left-1/2 w-[65.875rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
-      <div className="absolute top-1/2 left-1/2 w-[51.375rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
-      <div className="absolute top-1/2 left-1/2 w-[36.125rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
-      <div className="absolute top-1/2 left-1/2 w-[23.125rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2" />
+      <div className="rings-first-div -translate-x-1/2 -translate-y-1/2" />
+      <div className="rings-first-div  -translate-x-1/2 -translate-y-1/2" />
+      <div className="rings-first-div -translate-x-1/2 -translate-y-1/2" />
+      <div className="rings-first-div -translate-x-1/2 -translate-y-1/2" />
     </>
   );
 };
@@ -45,12 +45,12 @@ export const BackgroundCircles = ({ parallaxRef }: { parallaxRef?: any }) => {
   }, []);
 
   return (
-    <div className="absolute -top-[42.375rem] left-1/2 w-[78rem] aspect-square border border-n-2/5 rounded-full -translate-x-1/2 md:-top-[38.5rem] xl:-top-[32rem]">
+    <div className="backgroundCircles-main -translate-x-1/2">
       <Rings />
 
       {/* Moving background colored circle balls */}
       <MouseParallax strength={0.07} parallaxContainerRef={parallaxRef}>
-        <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom rotate-[46deg]">
+        <div className="mouseparallax-main rotate-46deg">
           <div
             className={`w-2 h-2 -ml-1 -mt-36 bg-gradient-to-b from-[#DD734F] to-[#1A1A32] rounded-full transition-transform duration-500 ease-out ${
               mounted ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
@@ -58,7 +58,7 @@ export const BackgroundCircles = ({ parallaxRef }: { parallaxRef?: any }) => {
           />
         </div>
 
-        <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom -rotate-[56deg]">
+        <div className="mouseparallax-main rotate-ms-56deg">
           <div
             className={`w-4 h-4 -ml-1 -mt-32 bg-gradient-to-b from-[#DD734F] to-[#1A1A32] rounded-full transition-transform duration-500 ease-out ${
               mounted ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
@@ -66,7 +66,7 @@ export const BackgroundCircles = ({ parallaxRef }: { parallaxRef?: any }) => {
           />
         </div>
 
-        <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom rotate-[54deg]">
+        <div className="mouseparallax-main rotate-54deg">
           <div
             className={`hidden w-4 h-4 -ml-1 mt-[12.9rem] bg-gradient-to-b from-[#B9AEDF] to-[#1A1A32] rounded-full xl:block transit transition-transform duration-500 ease-out ${
               mounted ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
@@ -74,7 +74,7 @@ export const BackgroundCircles = ({ parallaxRef }: { parallaxRef?: any }) => {
           />
         </div>
 
-        <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom -rotate-[65deg]">
+        <div className="mouseparallax-main rotate-ms-65deg">
           <div
             className={`w-3 h-3 -ml-1.5 mt-52 bg-gradient-to-b from-[#B9AEDF] to-[#1A1A32] rounded-full transition-transform duration-500 ease-out ${
               mounted ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
@@ -82,7 +82,7 @@ export const BackgroundCircles = ({ parallaxRef }: { parallaxRef?: any }) => {
           />
         </div>
 
-        <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom -rotate-[85deg]">
+        <div className="mouseparallax-main rotate-ms-58deg">
           <div
             className={`w-6 h-6 -ml-3 -mt-3 bg-gradient-to-b from-[#88E5BE] to-[#1A1A32] rounded-full transition-transform duration-500 ease-out ${
               mounted ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
@@ -90,7 +90,7 @@ export const BackgroundCircles = ({ parallaxRef }: { parallaxRef?: any }) => {
           />
         </div>
 
-        <div className="absolute bottom-1/2 left-1/2 w-0.25 h-1/2 origin-bottom rotate-[70deg]">
+        <div className="mouseparallax-main rotate-70deg">
           <div
             className={`w-6 h-6 -ml-3 -mt-3 bg-gradient-to-b from-[#88E5BE] to-[#1A1A32] rounded-full transition-transform duration-500 ease-out ${
               mounted ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
