052fabaa7516c31ab34bbd14bff907948cc516df
MD Morsed Alam <mdmorsedalam68@gmail.com>
2024-05-06 12:13:25 +0600

    working collaborate design

--- src/components/Collaboration.tsx
+++ src/components/Collaboration.tsx
@@ -4,25 +4,25 @@ import { collabApps, collabContent, collabText } from "../constants";
 import Button from "./Button";
 import Section from "./Section";
 import { LeftCurve, RightCurve } from "./design/RightLeftCurve";
-
+import "../assets/scss/Collaboration.scss";
 const Collaboration = () => {
   return (
     <Section crosses>
-      <div className="container lg:flex">
-        <div className="max-w-[25rem]">
-          <h2 className="h2 mb-4 md:mb-8">
+      <div className="container  collaborate-section">
+        <div className="left-side">
+          <h2>
             AI Chat App for seamless collaboration
           </h2>
 
-          <ul className="max-w-[22rem] mb-10 md:mb-14">
+          <ul>
             {collabContent.map((item) => (
-              <li className="mb-3 py-3" key={item.id}>
-                <div className="flex items-center">
+              <li key={item.id}>
+                <div>
                   <img src={check} width={24} height={24} alt="check" />
-                  <h6 className="body-2 ml-5">{item.title}</h6>
+                  <h6 className="body-2">{item.title}</h6>
                 </div>
                 {item.text && (
-                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
+                  <p className="body-2">{item.text}</p>
                 )}
               </li>
             ))}
@@ -31,8 +31,8 @@ const Collaboration = () => {
           <Button>Try it now</Button>
         </div>
 
-        <div className="lg:ml-auto xl:w-[38rem] mt-4">
-          <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+        <div className="right-side">
+          <p className="body-2">
             {collabText}
           </p>
 
