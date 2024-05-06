b2a13802766d332c7289b332a9209b4867027c60
MD Morsed Alam <mdmorsedalam68@gmail.com>
2024-05-06 13:30:35 +0600

    Collaboration design done

--- src/components/Collaboration.tsx
+++ src/components/Collaboration.tsx
@@ -36,10 +36,10 @@ const Collaboration = () => {
             {collabText}
           </p>
 
-          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
-            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
-              <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
-                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+          <div className="design-container">
+            <div className="design-content">
+              <div>
+                <div>
                   <img
                     src={brainwaveSymbol}
                     width={48}
@@ -53,18 +53,15 @@ const Collaboration = () => {
             <ul>
               {collabApps.map((app, index) => (
                 <li
+                style={{transform:`rotate(${index * 45 }deg)`}}
                   key={app.id}
-                  className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${
-                    index * 45
-                  }`}
+                  
                 >
                   <div
-                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
-                      index * 45
-                    }`}
+                  style={{transform:`rotate(-${index * 45 }deg)`}}
+                   
                   >
                     <img
-                      className="m-auto"
                       width={app.width}
                       height={app.height}
                       alt={app.title}
--- src/components/design/RightLeftCurve.tsx
+++ src/components/design/RightLeftCurve.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { curve1, curve2 } from "../../assets";
-
+import "../../assets/scss/Curve.scss";
 export const RightCurve = () => {
   return (
-    <div className="hidden absolute top-1/2 left-full w-[10.125rem] -mt-1 ml-10 pointer-events-none xl:block">
+    <div className="right-curve">
       <img src={curve2} width={162} height={76} alt="Curve 2" />
     </div>
   );
@@ -11,7 +11,7 @@ export const RightCurve = () => {
 
 export const LeftCurve = () => {
   return (
-    <div className="hidden absolute top-1/2 right-full w-[32.625rem] -mt-1 mr-10 pointer-events-none xl:block">
+    <div className="left-curve">
       <img src={curve1} width={522} height={182} alt="Curve 1" />
     </div>
   );
