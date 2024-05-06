a8fa9befd55e4cebebd0f892c51d412e5ec00401
MD Morsed Alam <mdmorsedalam68@gmail.com>
2024-05-06 17:18:18 +0600

    working on services section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -75,10 +75,10 @@ const Services = () => {
               <PhotoChatMessage />
             </div>
 
-            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
-              <div className="py-12 px-4 xl:px-8">
-                <h4 className="h4 mb-4">Video generation</h4>
-                <p className="body-2 mb-[2rem] text-n-3">
+            <div className="video">
+              <div className="video-content">
+                <h4>Video generation</h4>
+                <p className="body-2">
                   The world’s most powerful AI photo and video art generation
                   engine. What will you create?
                 </p>
@@ -87,16 +87,16 @@ const Services = () => {
                   {brainwaveServicesIcons.map((item, index) => (
                     <li
                       key={index}
-                      className={`rounded-2xl flex items-center justify-center ${
+                      className={` ${
                         index === 2
-                          ? "index-2-true"
-                          : "index-2-false"
+                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]"
+                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
                       }`}
                     >
                       <div
                         className={
                           index === 2
-                            ? "index-2-true"
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
                             : ""
                         }
                       >
@@ -107,9 +107,10 @@ const Services = () => {
                 </ul>
               </div>
 
-              <div className="video-img-container">
+              <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
                 <img
                   src={service3}
+                  className="w-full h-full object-cover"
                   width={520}
                   height={400}
                   alt="Scary robot"
